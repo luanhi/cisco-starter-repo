9b8e3ef760f8b3db6b71212f06efbacbec3d64ca
luanh@umich.edu <hill.luan@gmail.com>
2023-04-10 13:48:23 -0400

    Added banner and exhibit component

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { CCard, CCardBody } from '@coreui/react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">Sextant</div>
+      <div className="Exhibit">
+          <div><h1>Dashboard</h1></div>
+
+          <div className="Panel">IP</div>
+          <div className="Panel">Latency</div>
+      </div>
     </div>
   );
 }
